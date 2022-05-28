6fb088f02ca1c96f51fb8e4cd23f832f65c1f8ed
ValmiDrama <valjgyn@gmail.com>
2022-05-28 14:58:39 -0300

    Programacao em Python do básico ao avancado: Section 1 até 5

---
+++ dir_e_help.py
@@ -0,0 +1,13 @@
+"""
+#--------------------------------#
+#            dir/help
+#--------------------------------#
+Utilitários Python para que auxiliar na programação.
+dir -> Apresenta todos os atributos/propriedades e funções/métodos disponíveis
+para determinado tipo de dado ou variável.
+dir(tipo de dado/variável)
+help -> Apresenta a documentação/como utilizar os atributos/propriedades e funções/métodos disponíveis
+para determinado tipo de dado ou variável.
+help(tido de dado/variavel.propriedade)
+"""
+print(dir('geek'))
